f47c49d63fcfca88b331e36f1c2c2e878155b10d
Ivan-Yagilev <ivanrosk@mail.ru>
2023-05-14 15:29:10 +0300

    correct

---
+++ corr_methods.py
@@ -0,0 +1,152 @@
+from math import pi, sqrt, sin, cos, exp
+
+
+KSI = pi / 4
+
+
+def k(x):
+    if x < 0 or x > 1:
+        return 0
+    if x >= 0 and x < KSI:
+        return sqrt(2) * sin(x)
+    return cos(x) * cos(x)
+
+
+def q(x):
+    if x < 0 or x > 1:
+        return 0
+    if x >= 0 and x < KSI:
+        return 1
+    return x ** 2
+
+
+def f(x):
+    if x < 0 or x > 1:
+        return 0
+    if x >= 0 and x < KSI:
+        return sin(2*x)
+    return cos(x)
+
+
+def k_test(x):
+    if x < 0 or x > 1:
+        return 0
+    if x >= 0 and x < KSI:
+        return 1
+    return 0.5
+
+
+def q_test(x):
+    if x < 0 or x > 1:
+        return 0
+    if x >= 0 and x < KSI:
+        return 1
+    return KSI * KSI
+
+
+def f_test(x):
+    if x < 0 or x > 1:
+        return 0
+    if x >= 0 and x < KSI:
+        return 1
+    return sqrt(2)/2
+
+
+def analytic_solution(x):
+    if x < 0 or x > 1:
+        return 0
+
+    c1 = -0.3393176035227834
+    c2 = 0.3393176035227834
+    c3 = -0.4920418012319509
+    c4 = 1.0560782612867800
+
+    if x >= 0 and x <= KSI:
+        return 1 + c1*exp(x) + c2*exp(-x)
+    return c3*exp((pi*x)/(2*sqrt(2))) + c4*exp((-pi*x)/(2*sqrt(2))) + 8*sqrt(2)/(pi**2)
+
+
+def get_test_task_solution(n):
+    h = 1 / n
+    res = []
+
+    for i in range(n+1):
+        x = i * h
+        res.append(analytic_solution(x))
+    return res
+
+
+def get_integral(a, b, func):
+    h = (b - a) / 2
+    res = h / 3 * (func(a) + 4*func(a+h) + func(b))
+    return res
+
+
+def get_solution(n, k, q, f, border1, border2):
+    h = 1 / n
+
+    a_j = []
+    x = h
+    for _ in range(1, n+1):
+        if (x <= KSI or (x - h) >= KSI):
+            a_j.append(k(x-0.5*h))
+        else:
+            tmp = (KSI-x+h)/k(0.5*(x-h+KSI))+(x-KSI)/k(0.5*(x+KSI))
+            a_j.append(h / tmp)
+        x += h
+
+    d_j = []
+    fi_j = []
+    x = h
+    for _ in range(1, n):
+        if (x+0.5*h) <= KSI or (x-0.5*h) >= KSI:
+            d_j.append(n * get_integral(x-0.5*h, x+0.5*h, q))
+            fi_j.append(n * get_integral(x-0.5*h, x+0.5*h, f))
+            # d_j.append(q(x))
+            # fi_j.append(f(x))
+        else:
+            # d_j.append(n*(q(0.5*(x-0.5*h+KSI))*(KSI-x-0.5*h) + q(0.5*(KSI+x+0.5*h))*(x+0.5*h-KSI)))
+            # fi_j.append(n*(f(0.5*(x-0.5*h+KSI))*(KSI-x-0.5*h) + f(0.5*(KSI+x+0.5*h))*(x+0.5*h-KSI)))
+            d_j.append(n * (get_integral(x-0.5*h, KSI, q) + get_integral(KSI, x+0.5*h, q)))
+            fi_j.append(n * (get_integral(x-0.5*h, KSI, f) + get_integral(KSI, x+0.5*h, f)))
+        x += h
+
+    A, B, C = [], [], []
+    for i in range(n-1):
+        A.append(a_j[i] / (h**2))
+        B.append(a_j[i+1] / (h**2))
+        C.append(a_j[i] / (h**2) + a_j[i+1] / (h**2) + d_j[i])
+
+    xi1 = 0
+    # xi2 = 0
+    alpha = [xi1]
+    beta = [border1]
+    for i in range(n-1):
+        alpha.append(B[i]/(C[i] - A[i]*alpha[i]))
+        beta.append((fi_j[i] + A[i]*beta[i])/(C[i] - A[i]*alpha[i]))
+
+    v = [None] * (n + 1)
+    v[0] = border1
+    v[n] = border2
+    for i in range(n-1, 0, -1):
+        v[i] = alpha[i] * v[i+1] + beta[i]
+    return v
+
+
+def task(n, k, q, f, border1, border2):
+    v = get_solution(n, k, q, f, border1, border2)
+    v2 = get_solution(2*n, k, q, f, border1, border2)
+
+    e = []
+
+    x_tmp = 0.0
+    x = []
+    h = 1/n
+
+    v2 = [v2[2*i] for i in range(n+1)]
+    for i in range(n+1):
+        x.append(x_tmp)
+        e.append(abs(v[i] - v2[i]))
+        x_tmp += h
+
+    return x, v, v2, e
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-import methods
+from corr_methods import task, k, q, f, k_test, q_test, f_test, get_test_task_solution
 import matplotlib.pyplot as plt
 import csv
+plt.style.use('seaborn')
 
 
 def main(n):
-    x, v, v2, e = methods.numerical_main_task(n)
+    border1 = 1
+    border2 = 0
+    x, v, v2, e = task(n, k, q, f, border1, border2)
 
     with open('main.csv', 'w', newline='') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter=',',
@@ -18,52 +21,54 @@ def main(n):
     Задача решена с точностью ε2 = {max(e)};
     Максимальное отклонение аналитического и численного решений наблюдается в точке x= {x[e.index(max(e))]}""")
 
+    plt.subplot(1, 2, 1)
     plt.plot(x, v, 'b-', label='V(x)')
     plt.plot(x, v2, 'r-', label='V2(x)')
     plt.legend(loc='upper right')
-    plt.suptitle("Графики численных решений с обычным и половинным шагом")
-    plt.grid()
-    plt.show()
+    plt.title("Графики численных решений с обычным и половинным шагом")
 
+    plt.subplot(1, 2, 2)
     plt.plot(x, e, 'g-', label='v(x)-v2(x)')
     plt.legend(loc='upper right')
-    plt.suptitle("Разность графиков численного решений")
-    plt.grid()
+    plt.title("Разность графиков численных решений")
     plt.show()
 
 
 def test(n):
-    x, u, v_tmp, e = methods.numerical_test_task(n)
+    border1 = 1
+    border2 = 0
+    x, v, v2, e = task(n, k_test, q_test, f_test, border1, border2)
+    u = get_test_task_solution(n)
+    e = [abs(v[i]-u[i]) for i in range(len(v))]
 
     with open('test.csv', 'w', newline='') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter=',',
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
         spamwriter.writerow(['N', 'Xi', 'U(xi)', 'V(xi)', 'U(xi) - V(xi)'])
         for i in range(n+1):
-            spamwriter.writerow([i, x[i], u[i], v_tmp[i], e[i]])
+            spamwriter.writerow([i, x[i], u[i], v[i], e[i]])
 
     print(f"""    Для решения задачи использована равномерная сетка с числом разбиений n = {n};
     Задача должна быть решена с погрешностью не более ε = 0.5⋅10 –6;
     Задача решена с погрешностью ε1 = {max(e)};
     Максимальное отклонение аналитического и численного решений наблюдается в точке x = {x[e.index(max(e))]}""")
 
+    plt.subplot(1, 2, 1)
     plt.plot(x, u, 'b-', label='u')
-    plt.plot(x, v_tmp, 'r-', label='v_tmp')
+    plt.plot(x, v, 'r-', label='v')
     plt.legend(loc='upper right')
-    plt.suptitle("Графики аналитического и численного решений")
-    plt.grid()
-    plt.show()
+    plt.title("Графики аналитического и численного решений")
 
+    plt.subplot(1, 2, 2)
     plt.plot(x, e, 'g-', label='u(x)-v(x)')
     plt.legend(loc='upper right')
-    plt.suptitle("Разность графиков аналитического и численного решений")
-    plt.grid()
+    plt.title("Разность графиков аналитического и численного решений")
     plt.show()
 
 
 if __name__ == "__main__":
     choice = input("1. Тестовая задача, 2. Основная\n")
-    n = int(input("Введите число шагов\n"))
+    n = int(input("Введите число узлов\n"))
     if choice == '1':
         test(n)
     else:
